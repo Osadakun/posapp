9dafaeedc7774c911a9a7f4f68ffc9b75d683a05
Osadakun <b1915053@planet.kanazawa-it.ac.jp>
2022-02-03 02:59:41 +0900

    Firabase起動してくれ

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,15 @@
-import 'dart:html';
-
-import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 
 Future<void> main() async {
+  // Firebase初期化
   await Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -28,8 +28,11 @@ class MyAuthPage extends StatefulWidget {
 }
 
 class _MyAuthPageState extends State<MyAuthPage> {
+  // 入力されたメールアドレス
   String newUserEmail = "";
+  // 入力されたパスワード
   String newUserPassword = "";
+  // 登録・ログインに関する情報を表示
   String infoText = "";
 
   @override
@@ -40,7 +43,8 @@ class _MyAuthPageState extends State<MyAuthPage> {
           padding: EdgeInsets.all(32),
           child: Column(
             children: <Widget>[
-              TextField(
+              TextFormField(
+                // テキスト入力のラベルを設定
                 decoration: InputDecoration(labelText: "メールアドレス"),
                 onChanged: (String value) {
                   setState(() {
@@ -51,6 +55,7 @@ class _MyAuthPageState extends State<MyAuthPage> {
               const SizedBox(height: 8),
               TextFormField(
                 decoration: InputDecoration(labelText: "パスワード（６文字以上）"),
+                // パスワードが見えないようにする
                 obscureText: true,
                 onChanged: (String value) {
                   setState(() {
@@ -62,6 +67,7 @@ class _MyAuthPageState extends State<MyAuthPage> {
               ElevatedButton(
                 onPressed: () async {
                   try {
+                    // メール/パスワードでユーザー登録
                     final FirebaseAuth auth = FirebaseAuth.instance;
                     final UserCredential result =
                         await auth.createUserWithEmailAndPassword(
@@ -69,17 +75,19 @@ class _MyAuthPageState extends State<MyAuthPage> {
                       password: newUserPassword,
                     );
 
+                    // 登録したユーザー情報
                     final User user = result.user!;
                     setState(() {
                       infoText = "登録OK：${user.email}";
                     });
                   } catch (e) {
+                    // 登録に失敗した場合
                     setState(() {
                       infoText = "登録NG：${e.toString()}";
                     });
                   }
                 },
-                child: Text("ユーザ登録"),
+                child: Text("ユーザー登録"),
               ),
               const SizedBox(height: 8),
               Text(infoText)
